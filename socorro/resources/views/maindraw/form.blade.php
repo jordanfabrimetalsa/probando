cdd590e363fc05217c499a86a85256826ade10b0
jordanfabrimetalsa <jaguilera@fabrimetalsa.cl>
2025-09-01 11:00:24 -0400

    Se agrego fonodo a formulario

--- socorro/resources/views/maindraw/form.blade.php
+++ socorro/resources/views/maindraw/form.blade.php
@@ -1,6 +1,6 @@
 <div class="modal fade" id="avisoModal" tabindex="-1" aria-labelledby="avisoModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
+    <div class="modal-dialog modal-xl">
+      <div class="modal-content modal-extra-background">
         <div class="modal-header">
           <h5 class="modal-title" id="avisoModalLabel">Registro de Salida.</h5>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
